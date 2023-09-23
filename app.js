26fde76032bc1b92841c79d1674bdc3969efe6d0
muhammadimam155 <imamaliatari@gmail.com>
2023-09-24 00:12:39 +0500

    alert

---
+++ app.js
@@ -0,0 +1,4 @@
+alert("welcome to my website")
+alert("error! please enter a valid password")
+alert("welcome to js land... \nhappy coding! ")
+console.log("HELLO.... i can throught this my brosser consol")
